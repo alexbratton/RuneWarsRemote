3b51773a3f387585e21a0d90865939200e1e4e9b
Alex Bratton <alex@bratton.com>
2020-06-23 10:26:07 -0500

    Initial Commit

---
+++ RuneWarsRemote/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RuneWarsRemote
+//
+//  Created by Alex Bratton on 6/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RuneWarsRemote/RuneWarsRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuneWarsRemoteApp.swift
+//  RuneWarsRemote
+//
+//  Created by Alex Bratton on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct RuneWarsRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
